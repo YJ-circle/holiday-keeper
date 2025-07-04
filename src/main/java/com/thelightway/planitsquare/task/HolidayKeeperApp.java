f6fa394b6fafffe4e733e1381feb6ecd7e6b1ebd
thelightway <dev@thelightway.kr>
2025-07-04 11:54:38 +0900

    기본 환경 설정 추가
    * 스프링 기본 클래스 명 수정
    * application.properties -> application.yml로 변경
    * H2 DB 환경 설정 추가
    * logback 로그 환경 설정 추가
     - File Appender(max-file-size: 5MB)
     - 기본 로그 설정 레벨 INFO

--- src/main/java/com/thelightway/planitsquare/task/HolidayKeeperApp.java
+++ src/main/java/com/thelightway/planitsquare/task/HolidayKeeperApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskApplication {
+public class HolidayKeeperApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskApplication.class, args);
+		SpringApplication.run(HolidayKeeperApp.class, args);
 	}
 
 }

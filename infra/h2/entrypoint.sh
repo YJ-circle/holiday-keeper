f33d7590b626d57f1ebb8ef9c7e63ee8b6c51756
thelightway <dev@thelightway.kr>
2025-07-05 14:06:31 +0900

    Chore: entrypoint.sh 파일 못찾는 문제 수정, Batch sql 추가

--- infra/h2/entrypoint.sh
+++ infra/h2/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 DATA_DIR=/opt/h2-data
 
-if ! ls "$DATA_DIR"/*.db; then
+if ! ls "$DATA_DIR"/test.mv.db; then
   echo "데이터베이스 초기 설정 실행.. /sql/init.sql 실행"
   java -cp /opt/h2/bin/h2*.jar org.h2.tools.RunScript \
     -url "jdbc:h2:file:$DATA_DIR/test" \
     -user sa \
     -script /sql/init.sql
+  echo "초기 설정 실행 완료"
+  
+  echo "Batch 테이블 추가.. /sql/batch-h2.sql 실행"
+  java -cp /opt/h2/bin/h2*.jar org.h2.tools.RunScript \
+    -url "jdbc:h2:file:$DATA_DIR/test" \
+    -user sa \
+    -script /sql/batch-h2.sql
   echo "초기 설정 실행 완료"
 fi
 
@@ -16,4 +23,3 @@ exec java -cp /opt/h2/bin/h2*.jar org.h2.tools.Server \
   -web -webAllowOthers \
   -baseDir "$DATA_DIR" \
   -ifNotExists
-  

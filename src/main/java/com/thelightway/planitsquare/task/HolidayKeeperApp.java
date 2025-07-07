f97e7cfc0952a1be5998d0a49118ede6585a3878
thelightway <dev@thelightway.kr>
2025-07-07 11:50:38 +0900

    Chore: Swagger 의존성 추가

--- src/main/java/com/thelightway/planitsquare/task/HolidayKeeperApp.java
+++ src/main/java/com/thelightway/planitsquare/task/HolidayKeeperApp.java
@@ -2,8 +2,19 @@ package com.thelightway.planitsquare.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@OpenAPIDefinition(
+	info = @Info(
+		title       = "Holiday Keeper API",
+		version     = "1.0.0"
+	)
+)
 public class HolidayKeeperApp {
 
 	public static void main(String[] args) {

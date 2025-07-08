6b4ec1f7d72bbc9842bbfc35e837bc06e830e1e5
thelightway <dev@thelightway.kr>
2025-07-08 23:23:11 +0900

    Refactor: 배치 작업 요청 추상 클래스 추가로 인한 RequestMapping 변경

--- src/main/java/com/thelightway/planitsquare/task/scraper/country/controller/CountryScraperCtrl.java
+++ src/main/java/com/thelightway/planitsquare/task/scraper/country/controller/CountryScraperCtrl.java
@@ -3,26 +3,24 @@ package com.thelightway.planitsquare.task.scraper.country.controller;
 import static com.thelightway.planitsquare.task.common.response.ApiResponse.*;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thelightway.planitsquare.task.scraper.AbstractScraperController;
 import com.thelightway.planitsquare.task.scraper.country.service.CountryScraperService;
 
 import lombok.RequiredArgsConstructor;
 
-
 @RestController
-@RequestMapping("/scraper")
 @RequiredArgsConstructor
-public class CountryScraperCtrl {
-	private final CountryScraperService countryScraperService;
+public class CountryScraperCtrl extends AbstractScraperController {
 	private static final String REQUEST_SUCCESS_MESSAGE = "국가 수집 요청이 정상적으로 전달되었습니다.";
 	private static final String ALREADY_RUNNING_MESSAGE = "국가 수집이 이미 실행 중입니다. 나중에 다시 시도 하세요";
+	private final CountryScraperService countryScraperService;
 
-	@GetMapping("/country/start")
+	@PostMapping("/country")
 	public ResponseEntity requestCountryScrap() {
-		if(countryScraperService.startCountryScrap()){
+		if (countryScraperService.startCountryScrap()) {
 			return success(REQUEST_SUCCESS_MESSAGE);
 		}
 		return success(ALREADY_RUNNING_MESSAGE);

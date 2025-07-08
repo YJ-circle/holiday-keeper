588b943d4e155bc00a2aed517f48210c6737bb10
thelightway <dev@thelightway.kr>
2025-07-08 23:08:19 +0900

    Feat: 공휴일 데이터 수집 배치 API 추상 클래스 추가

---
+++ src/main/java/com/thelightway/planitsquare/task/scraper/AbstractScraperController.java
@@ -0,0 +1,7 @@
+package com.thelightway.planitsquare.task.scraper;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/scraper")
+public abstract class AbstractScraperController {
+}

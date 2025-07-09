d3306e9ac96c539ccb86837ad001dc6f0204ecc8
thelightway <dev@thelightway.kr>
2025-07-09 17:40:42 +0900

    Refactor: 소프트 삭제 메소드명 수정 및 주석 추가

--- src/main/java/com/thelightway/planitsquare/task/common/entity/SoftDeletedEntity.java
+++ src/main/java/com/thelightway/planitsquare/task/common/entity/SoftDeletedEntity.java
@@ -6,14 +6,21 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 소프트 삭제를 위한 Base Entity입니다.
+ * active 값이
+ * 	true이면 활성 상태를,
+ * 	false이면 비활성 상태를 의미합니다.
+ * 일반적으로 active 값이 true인 값만 서비스에서 조회가 가능합니다.
+ */
 @MappedSuperclass
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class SoftDeletedEntity extends AuditBaseEntity{
+public abstract class SoftDeletedEntity extends AuditBaseEntity {
 	@Column(name = "active")
 	private boolean active = true;
 
-	public void changeDeleteStatus(boolean active) {
+	public void changeActiveStatus(boolean active) {
 		this.active = active;
 	}
 }

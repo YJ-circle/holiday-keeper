4a2d18c8e820eb90e100323492e551be7840463f
thelightway <dev@thelightway.kr>
2025-07-09 17:53:08 +0900

    Feat: DateUtils 추가

---
+++ src/main/java/com/thelightway/planitsquare/task/common/utils/DateUtils.java
@@ -0,0 +1,17 @@
+package com.thelightway.planitsquare.task.common.utils;
+
+import java.time.LocalDate;
+
+public class DateUtils {
+	public static LocalDate firstDayByYear(int year) {
+		return LocalDate.of(year, 1, 1);
+	}
+
+	public static LocalDate lastDayByYear(int year) {
+		return LocalDate.of(year, 12, 31);
+	}
+
+	public static int getCurrentYear() {
+		return LocalDate.now().getYear();
+	}
+}
